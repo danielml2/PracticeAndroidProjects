f62ef437145c76cffa0775e3f2c928226e51646e
Daniel <officialcodedevelop@gmail.com>
2022-09-12 11:33:07 +0300

    Added DialogBox for Invalid name input & TTS

--- app/src/main/java/me/danielml/schoolmemorygame/GameActivity.java
+++ app/src/main/java/me/danielml/schoolmemorygame/GameActivity.java
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.TextView;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
 
     private GridLayout gridLayout;
     private TextView player1NameTv;
     private TextView player2NameTv;
+
+    private Button exitGameBtn;
 
     private String player1Name = "Player 1", player2Name = "Player 2";
 
@@ -23,8 +27,11 @@ public class GameActivity extends AppCompatActivity {
 
         this.player1NameTv = findViewById(R.id.player1NameTv);
         this.player2NameTv =  findViewById(R.id.player2NameTv);
+        this.exitGameBtn = findViewById(R.id.exitGameBtn);
 
         Intent intent = getIntent();
+
+        exitGameBtn.setOnClickListener(this);
 
 
         if(intent != null) {
@@ -35,5 +42,15 @@ public class GameActivity extends AppCompatActivity {
 
         player1NameTv.setText(player1Name);
         player2NameTv.setText(player2Name);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == exitGameBtn.getId()) {
+            Intent intent = new Intent(this, MainActivity.class);
+
+            startActivity(intent);
+        }
+
     }
 }
--- app/src/main/java/me/danielml/schoolmemorygame/MainActivity.java
+++ app/src/main/java/me/danielml/schoolmemorygame/MainActivity.java
@@ -2,11 +2,16 @@ package me.danielml.schoolmemorygame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -15,6 +20,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private EditText player1NameInput;
     private EditText player2NameInput;
+
+    private TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,18 +34,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         player1NameInput = (EditText) findViewById(R.id.playerName1Input);
         player2NameInput = (EditText) findViewById(R.id.playerName2Input);
+
+        this.tts = new TextToSpeech(getApplicationContext(), (i) -> {
+            if(i == TextToSpeech.SUCCESS)
+                tts.setLanguage(Locale.ENGLISH);
+        });
+
     }
 
     @Override
     public void onClick(View v) {
 
         if(v.getId() == playBtn.getId()) {
-            Intent intent = new Intent(this, GameActivity.class);
+            String name1Input = player1NameInput.getText().toString().trim();
+            String name2Input = player2NameInput.getText().toString().trim();
 
-            intent.putExtra("player1Name", player1NameInput.getText().toString());
-            intent.putExtra("player2Name", player2NameInput.getText().toString());
+            if(name1Input.length() <= 0 || name2Input.length() <= 0 || name1Input.equals(name2Input)) {
+                new AlertDialog.Builder(this)
+                        .setMessage("Invalid names input! Names can't be equal to one another or 0 characters long")
+                        .setNeutralButton("OK", (dialog, which) -> { })
+                        .setTitle("Error!")
+                        .setIcon(R.drawable.error)
+                        .show();
+            } else {
+                Intent intent = new Intent(this, GameActivity.class);
 
-            startActivity(intent);
+                tts.speak("Enjoy and Good Luck! " + name1Input + " and " + name2Input, TextToSpeech.QUEUE_FLUSH, null);
+
+                intent.putExtra("player1Name", player1NameInput.getText().toString());
+                intent.putExtra("player2Name", player2NameInput.getText().toString());
+
+                startActivity(intent);
+            }
+
         }
     }
 }
